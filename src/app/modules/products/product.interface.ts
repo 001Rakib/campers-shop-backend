05e57c015d2579a1445e6fdfe3e4f95386da4ee7
001Rakib <rakibrakibrakib354@gmail.com>
2024-07-08 23:45:22 +0600

    created product model and interface

---
+++ src/app/modules/products/product.interface.ts
@@ -0,0 +1,12 @@
+type TProduct = {
+  name: string;
+  price: number;
+  stock: number;
+  quantity: number;
+  description: string;
+  category: string;
+  ratings: number;
+  images: string;
+};
+
+export default TProduct;
---
+++ src/app/modules/products/product.model.ts
@@ -0,0 +1,47 @@
+import { model, Schema } from "mongoose";
+import TProduct from "./product.interface";
+
+const productSchema = new Schema<TProduct>({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  ratings: {
+    type: Number,
+    min: 0,
+    max: 5,
+    default: 0,
+  },
+  images: {
+    type: String,
+    required: true,
+  },
+});
+
+export const Product = model<TProduct>("Product", productSchema);
